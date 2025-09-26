cb088a6cc1e345526f56e02dcd47c71cf9363793
47mango <do_hh@naver.com>
2025-09-26 21:18:13 +0900

    fix: cleanup malicious payload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center h-screen w-full'>
+      <h1>7기 모집사이트 프론트엔드 입니다.</h1>
+    </div>
+  )
+}
+
+export default App
