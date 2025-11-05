bd6ad0ed595ea6b6bed61011a1d7bbf0b5ac79cd
최혜림 <101788177+hyerim03@users.noreply.github.com>
2025-11-06 02:11:37 +0900

    feat: 포지션 선택 기능 추가

--- src/page/form/SubmitForm.tsx
+++ src/page/form/SubmitForm.tsx
@@ -5,6 +5,7 @@ import LongButton from "../../components/button/LongButton";
 
 const SubmitForm = () => {
   const navigate = useNavigate();
+
   return (
     <div className="flex flex-col items-center justify-center h-screen px-4">
       <div className="rounded-full bg-[#4173FF] w-[49px] h-[49px] flex items-center justify-center">
@@ -40,7 +41,7 @@ const SubmitForm = () => {
       </div>
       <LongButton
         text="처음으로 돌아가기"
-        onClickHandler={() => navigate("/chat")}
+        onClickHandler={() => navigate("/")}
       />
     </div>
   );
