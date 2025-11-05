6d04f07d7c044bd388d6eee4f57d72582b27f379
47mango <do_hh@naver.com>
2025-11-06 00:13:49 +0900

    fix : 전송 버튼 고정

--- src/components/InputArea.tsx
+++ src/components/InputArea.tsx
@@ -29,7 +29,7 @@ export default function InputArea({ onSend, status }: InputAreaProps) {
         />
         <button
           onClick={handleSend}
-          className="h-14 w-14 bg-[#4173FF] rounded-full flex items-center justify-center hover:bg-blue-600 transition-colors"
+          className="h-[56px] w-[56px] bg-[#4173FF] rounded-full flex items-center justify-center hover:bg-blue-600 transition-colors"
         >
           <img src={Send} className="h-8 w-8" alt="Send" />
         </button>
