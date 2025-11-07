e3ac11bfbb3a20095297fbfcdb3bbd2908a8d940
최혜림 <101788177+hyerim03@users.noreply.github.com>
2025-11-07 14:49:53 +0900

    fix: cleanup malicious payload

--- src/components/form/TopBar.tsx
+++ src/components/form/TopBar.tsx
@@ -1,5 +1,3 @@
-import { useNavigate } from "react-router-dom";
-
 const TopBar = () => {
   return (
     <div className="h-[73px] flex items-center px-[27px] border border-x-0 border-t-0 border-b-[#e4e4e4]">
